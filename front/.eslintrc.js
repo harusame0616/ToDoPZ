ed6ec7d6b3d13e562c9581a064df842e9e2bf2dc
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2022-11-12 22:47:02 +0900

    chore: storybook導入

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'plugin:vue/vue3-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
   ],
   overrides: [],
   parser: '@typescript-eslint/parser',
---
+++ front/.storybook/main.js
@@ -0,0 +1,26 @@
+module.exports = {
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../**/components/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
+  framework: '@storybook/vue3',
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  features: {
+    storyStoreV7: true,
+  },
+};
